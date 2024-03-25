f86baadee481f80e4cdef1e19c7093d197fdd5e6
Natural-Horse <164884831@qq.com>
2024-03-25 11:41:27 +0800

    docs(实习开发日志.md): 更新日志
    对3.25之前的开发日志进行汇总

--- hello.go
+++ hello.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
 	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello World!")
+}
 
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello World!")
-	})
+
+	//middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	//routes
+	e.GET("/", hello)
+
+	//start server
 	e.Logger.Fatal(e.Start(":2022"))
 }
